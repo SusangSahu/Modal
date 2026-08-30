e2f5d05cdbfb6902373beb090974e85394970e00
Susang Kumar Sahu <sahususang7978@gmail.com>
2026-08-30 22:41:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const model = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+const closeModal = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape' && !model.classList.contains('hidden')) {
+    closeModal();
+  }
+});
